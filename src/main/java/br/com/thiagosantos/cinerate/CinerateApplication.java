ba112615e94a794c36ce40ab8b879f0c18b75d1f
Thiago Henrique <thiaghenrique145@gmail.com>
2025-10-12 14:03:35 -0300

    Feat: commit inicial

---
+++ src/main/java/br/com/thiagosantos/cinerate/CinerateApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagosantos.cinerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinerateApplication.class, args);
+	}
+
+}
