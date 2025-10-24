9ca77391b6a1edcfb58d4d2b256026fe10c54c57
Thiago Henrique <thiago.1983@aluno.pr.senac.br>
2025-10-24 20:19:31 -0300

    Feat: CRUD das rotas Avaliacao, Genero e Obra

--- backend/src/main/java/br/com/thiagosantos/cinerate/controller/AvaliacaoController.java
+++ backend/src/main/java/br/com/thiagosantos/cinerate/controller/AvaliacaoController.java
@@ -1,7 +1,57 @@
 package br.com.thiagosantos.cinerate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.thiagosantos.cinerate.entities.Avaliacao;
+import br.com.thiagosantos.cinerate.repository.AvaliacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AvaliacaoController {
+    @Autowired
+    AvaliacaoRepository avalicaoRepository;
+
+    @RequestMapping(value = "listar", method = RequestMethod.GET)
+    public ResponseEntity<List<Avaliacao>> listar() {
+        return ResponseEntity.ok((List<Avaliacao>) avalicaoRepository.findAll());
+    }
+
+    @RequestMapping(value = "listar/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Avaliacao> getById(@PathVariable(value = "id") Long id) {
+        Optional<Avaliacao> object = avalicaoRepository.findById(id);
+        if (object.isPresent()) {
+            return new ResponseEntity<>(object.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "novo", method = RequestMethod.POST)
+    public ResponseEntity<Avaliacao> novo(@RequestBody Avaliacao avaliacao) {
+        return new ResponseEntity<Avaliacao>(avalicaoRepository.save(avaliacao), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "remover/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Avaliacao> remover(@PathVariable(value = "id") Long id) {
+        Optional<Avaliacao> object = avalicaoRepository.findById(id);
+        if (object.isPresent()) {
+            avalicaoRepository.delete(object.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value = "atualizar/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Avaliacao> atualizar(@PathVariable(value = "id") Long id,
+                                               Avaliacao novaAvaliacao) {
+        Optional<Avaliacao> object = avalicaoRepository.findById(id);
+        if (object.isPresent()) {
+            return new ResponseEntity<>(avalicaoRepository.save(novaAvaliacao), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
--- backend/src/main/java/br/com/thiagosantos/cinerate/controller/GeneroController.java
+++ backend/src/main/java/br/com/thiagosantos/cinerate/controller/GeneroController.java
@@ -1,7 +1,57 @@
 package br.com.thiagosantos.cinerate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.thiagosantos.cinerate.entities.Genero;
+import br.com.thiagosantos.cinerate.repository.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class GeneroController {
+    @Autowired
+    GeneroRepository generoRepository;
+
+    @RequestMapping(value = "listar", method = RequestMethod.GET)
+    public ResponseEntity<List<Genero>> listar() {
+        return ResponseEntity.ok((List<Genero>) generoRepository.findAll());
+    }
+
+    @RequestMapping(value = "listar/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Genero> getById(@PathVariable(value = "id") Long id) {
+        Optional<Genero> object = generoRepository.findById(id);
+        if (object.isPresent()) {
+            return new ResponseEntity<>(object.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "novo", method = RequestMethod.POST)
+    public ResponseEntity<Genero> novo(@RequestBody Genero genero) {
+        return new ResponseEntity<Genero>(generoRepository.save(genero), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "remover/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Genero> remover(@PathVariable(value = "id") Long id) {
+        Optional<Genero> object = generoRepository.findById(id);
+        if (object.isPresent()) {
+            generoRepository.delete(object.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(value = "atualizar/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Genero> atualizar(@PathVariable(value = "id") Long id,
+                                            Genero novoGenero) {
+        Optional<Genero> object = generoRepository.findById(id);
+        if (object.isPresent()) {
+            return new ResponseEntity<>(generoRepository.save(novoGenero), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
