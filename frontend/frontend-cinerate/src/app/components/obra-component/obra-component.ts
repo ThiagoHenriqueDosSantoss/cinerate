13379be89699f38a6be38161c246c17b3be1ded6
Thiago Henrique <thiaghenrique145@gmail.com>
2025-11-02 20:27:56 -0300

    Feat: Criando componentes, html e css para obras

---
+++ frontend/frontend-cinerate/src/app/components/obra-component/obra-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obra-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './obra-component.html',
+  styleUrl: './obra-component.css'
+})
+export class ObraComponent {
+
+}
