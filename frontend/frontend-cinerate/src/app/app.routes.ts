d3ba5e73564d44129b21eef7743641fb869299cd
Thiago Henrique <thiaghenrique145@gmail.com>
2025-11-02 21:24:34 -0300

    Feat: Criando components obra, home e usuarios e realizando a trativa de rota dos mesmos

--- frontend/frontend-cinerate/src/app/app.routes.ts
+++ frontend/frontend-cinerate/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { ObraComponent } from './components/obra-component/obra-component';
+import { UsuarioComponent } from './components/usuario-component/usuario-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent,
+        children: [
+            { path: 'obras', component: ObraComponent },
+            { path: 'usuarios', component: UsuarioComponent },
+        ],
+    }
+];
---
+++ frontend/frontend-cinerate/src/app/components/home-component/home-component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+//Imports do primeng
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-home-component',
+  standalone: true,
+  imports: [MenubarModule,RouterLink],
+  templateUrl: './home-component.html',
+  styleUrl: './home-component.css',
+})
+export class HomeComponent implements OnInit{
+  items: MenuItem[] | undefined;
+
+  constructor(private router: Router){}
+  ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+                routerLink:  ['/home'] 
+            },
+            {
+                label: 'Obras',
+                icon: 'pi pi-lightbulb',
+                routerLink:  ['/home/obras'] 
+            },
+            {
+                label: 'Usuarios',
+                icon: 'pi pi-users',
+                routerLink:  ['/home/usuarios'] 
+            },
+        ]
+    }
+}
